fae16f39eeef779d313a35cbf4297d14682becf1
Bishal Saha <bishal.saha@gmail.com>
2018-12-25 21:16:48 +0530

    deleted the unwanted folders and igonred the logs folder

--- cp/resources/views/backend/errors/403.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.errors')
-
-@section('title', 'Forbidden!')
-
-@section('content')
-    <h1 class="mt-5">Forbidden!</h1>
-    <p class="lead">You don't have permission to access this page.</p>
-@stop
--- cp/resources/views/backend/errors/503.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.errors')
-
-@section('title', 'Be Right Back')
-
-@section('content')
-    <h1 class="mt-5">Be Right Back.</h1>
-    <p class="lead">Application is currently being updated.  Please check back in few minutes.</p>
-@stop
